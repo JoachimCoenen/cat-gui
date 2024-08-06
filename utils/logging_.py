ba0ffe9a28d681d55f9bf3993dc7fbe976f77a1d
JoachimCoenen <joachimcoenen@icloud.com>
2024-08-06 08:55:17 +0200

    added loggingIndentError, loggingIndentFatal

--- utils/logging_.py
+++ utils/logging_.py
@@ -245,6 +245,8 @@ loggingIndentInfo: _LoggingIndentFunction = _loggingIndent('loggingIndentInfo', 
 """contextmanager that increases th indentation for all contained logging operations if the log level is greater or equal to LogLevel.INFO."""
 loggingIndentWarning: _LoggingIndentFunction = _loggingIndent('loggingIndentWarning', LogLevel.WARN)
 """contextmanager that increases th indentation for all contained logging operations if the log level is greater or equal to LogLevel.WARN."""
+loggingIndentError: _LoggingIndentFunction = _loggingIndent('loggingIndent', LogLevel.ERROR)
+"""contextmanager that increases th indentation for all contained logging operations if the log level is greater or equal to LogLevel.ERROR."""
 loggingIndentFatal: _LoggingIndentFunction = _loggingIndent('loggingIndent', LogLevel.FATAL)
 """contextmanager that increases th indentation for all contained logging operations if the log level is greater or equal to LogLevel.FATAL."""
 loggingIndent: _LoggingIndentFunction = _loggingIndent('loggingIndent', LogLevel.ALWAYS)
@@ -287,6 +289,8 @@ __all__ = [
 	"loggingIndentDebug",
 	"loggingIndentInfo",
 	"loggingIndentWarning",
+	"loggingIndentError",
+	"loggingIndentFatal",
 	"loggingIndent",
 
 	"setLoggingStream",
